6c973a3b9018699801cb72abf0748696e326883f
Haoye <haoyetang@gmail.com>
2020-10-04 01:03:10 -0400

    Attempt to implement Updates section

--- lib/updates.dart
+++ lib/updates.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
+import 'package:flutter_custom_tabs/flutter_custom_tabs.dart' as custom;
 import 'package:http/http.dart' as http;
+import 'package:metadata_fetch/metadata_fetch.dart';
 
 class Updates extends StatefulWidget {
   @override
@@ -11,65 +12,85 @@ class Updates extends StatefulWidget {
 }
 
 class UpdatesState extends State<Updates> {
-  var urlList;
   int urlAmount;
   List<String> urls;
-  void _parseUrlList() async {
-    urlList = await http.read('https://raw.githubusercontent.com/howyay/snapp/master/updates.txt?token=AGYNE7VQA6QO7VIRW2A5AJK7QJAZ2');
+  String url;
+  String title;
+
+  Future<String> _parseUrlList() async {
+    var urlList = await http.read(
+        'https://raw.githubusercontent.com/howyay/snapp/master/updates.txt?token=AGYNE7VQA6QO7VIRW2A5AJK7QJAZ2');
     urls = new LineSplitter().convert(urlList);
     urlAmount = urls.length;
   }
 
-  String url;
+  Future<String> _getTitle(int index) async {
+    var response = await http.get(urls[index]);
+    var document = responseToDocument(response);
+    var data = MetadataParser.htmlMeta(document);
+    return title = data.title;
+  }
 
   @override
   Widget build(BuildContext context) {
-    _parseUrlList();
-    return Container(
-      child: ListView.builder(
-        itemCount: urlAmount,
-        itemBuilder: (context, index) {
-          return GestureDetector(
-            onTap: () => {
-              print('retard'),
-              url = urls[index],
-              _launchURL(context)
+    // return FutureBuilder(
+    //   future: _parseUrlList(),
+    //   builder: (context, snapshot) {
+    //     return Container(
+    //       child: ListView.builder(
+    //         itemCount: urlAmount,
+    //         itemBuilder: (context, index) {
+    //           return GestureDetector(
+    //             onTap: () async {
+    //               await custom.launch(
+    //                 '$url',
+    //                 option: new custom.CustomTabsOption(
+    //                   toolbarColor: Theme.of(context).primaryColor,
+    //                   enableDefaultShare: true,
+    //                   enableUrlBarHiding: true,
+    //                   showPageTitle: true,
+    //                   animation: new custom.CustomTabsAnimation.slideIn(),
+    //                   extraCustomTabs: <String>[
+    //                     // ref. https://play.google.com/store/apps/details?id=org.mozilla.firefox
+    //                     'org.mozilla.firefox',
+    //                     // ref. https://play.google.com/store/apps/details?id=com.microsoft.emmx
+    //                     'com.microsoft.emmx',
+    //                   ],
+    //                 ),
+    //               );
+    //             },
+    //             child: Card(
+    //               child: Column(
+    //                 children: [
+    //                   FutureBuilder(
+    //                     future: _getTitle(index),
+    //                     builder: (context, snapshot) {
+    //                       return Text(title);
+    //                     },
+    //                   ),
+    //                 ],
+    //               ),
+    //             ),
+    //           );
+    //         },
+    //       ),
+    //     );
+    //   },
+    // );
+    return FutureBuilder(
+      future: _parseUrlList(),
+      builder: (context,snapshot) {
+        return Container(
+          child: ListView.builder(
+            itemCount: urlAmount,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Text('${urls[index]}'),
+              );
             },
-            child: Card(
-              child: Column(
-                children: [
-                  Text(urls[index]),
-                ],
-              ),
-            ),
-          );
-        },
-      )
+          ),
+        );
+      },
     );
-  }
-
-  void _launchURL(BuildContext context) async {
-    try {
-      await launch(
-        '$url',
-        option: new CustomTabsOption(
-          toolbarColor: Theme.of(context).primaryColor,
-          enableDefaultShare: true,
-          enableUrlBarHiding: true,
-          showPageTitle: true,
-          animation: new CustomTabsAnimation.slideIn(),
-          // or user defined animation.
-          extraCustomTabs: <String>[
-            // ref. https://play.google.com/store/apps/details?id=org.mozilla.firefox
-            'org.mozilla.firefox',
-            // ref. https://play.google.com/store/apps/details?id=com.microsoft.emmx
-            'com.microsoft.emmx',
-          ],
-        ),
-      );
-    } catch (e) {
-      // An exception is thrown if browser app is not installed on Android device.
-      debugPrint(e.toString());
-    }
   }
 }
